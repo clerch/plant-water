9b4e114f464e2eee618e3965f093d2394a186220
ceebs <cthiessen@gmail.com>
2016-05-20 03:53:58 +0000

    radio seletors on add plant form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
 
   validates :username, uniqueness: true, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /.*@.*\..*/, message: "a valid email is required" }
-  validates :phone, presence: true, uniqueness: true
+  validates :phone, presence: true
   validates :password, presence: true
 
 end
+
