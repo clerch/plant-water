fcc0af5b5f2d89c548cb8062823f835fd873d382
clerch <lerchc@gmail.com>
2016-05-19 20:31:53 -0700

    actions updated

--- app/actions.rb
+++ app/actions.rb
@@ -3,6 +3,12 @@
 
 require_relative 'user_actions'
 
+helpers do 
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   erb :index
 end
@@ -11,4 +17,25 @@ get '/all-plants' do
   @plant_types = PlantType.all
   erb :'plants/index'
 end
+
+post '/add-plant' do
+    @new_plant = Plant.create(
+    user_id: current_user.id,
+    plant_type_id: params[:plant_type_id],
+    custom_name: params[:custom_name],
+    last_date_watered: params[:last_date_watered],
+    custom_watering_frequency: params[:custom_watering_frequency]
+  )
+  #if @post validates, save
+  if @new_plant.save
+    redirect(back)
+  else
+    #if it doesn't validate, print error messages
+  erb(:"posts/new") 
+  end
+
+
+end
+
+
 
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -5,6 +5,10 @@ class Plant < ActiveRecord::Base
   has_many :notifications
 
 
+  def average_water_frequency
+    ((plant_type.high_water_frequency + plant_type.low_water_frequency)/2).to_i
+  end
+
 
   def needs_watering?
     Date.today == (self.last_date_watered + self.custom_watering_frequency)
@@ -23,9 +27,5 @@ class Plant < ActiveRecord::Base
     # TODO: only do the following when the twilio call works out well
     Notification.new(user_id: user.id, message: message_body)
   end
-
-
-
-
 
 end
