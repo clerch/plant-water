7ef81ef0e03837039ffa336bd848a4f287f9c65d
clerch <lerchc@gmail.com>
2016-05-19 19:00:43 -0700

    minor change

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -12,7 +12,7 @@ class Notification < ActiveRecord::Base
     @client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
     message = @client.account.messages.create(
     body: 'Hello from the SKYNET!',
-    to: '+16046174988',    # Replace with your phone number
+    to: user.phone,    # Replace with your phone number
     from: ENV['COMPANY_PHONE'])  # Replace with your Twilio number
 
     puts message.sid
