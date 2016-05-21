46804c59654e55e6c96f7c047bdc126b9255e4b4
clerch <lerchc@gmail.com>
2016-05-21 15:13:48 -0700

    added update to actions/changed sort order

--- app/actions.rb
+++ app/actions.rb
@@ -15,30 +15,8 @@ get '/' do
 end
 
 get '/all-plants' do
-  @plant_types = PlantType.all
+  @plant_types = PlantType.all.order(common_name: :asc)
   erb :'plants/index'
 end
 
-post '/plant-add' do
-  @new_plant = Plant.new(
-    user_id: session[:user_id],
-    plant_type_id: params[:plant_type_id],
-    custom_name: params[:custom_name],
-    last_date_watered: params[:last_date_watered],
-    custom_watering_frequency: params[:custom_watering_frequency]
-  )
-    @new_plant.calculate_next_water_date
-    @new_plant.save
-  #if @new_plant validates, save
-  if @new_plant.save
-    if request.xhr?
-      json  :plant => {  new_plant:  @new_plant, 
-                         common_name: @new_plant.plant_type.common_name
-                      }
-    else
-      redirect(back)
-    end
-  else
-    erb :'plants/show'
-  end
-end
+
--- app/user_actions.rb
+++ app/user_actions.rb
@@ -1,23 +1,4 @@
-# require 'phony'
 
-# Not used
-# def convert_to_e164(raw_phone)
-#   Phony.format(
-#     raw_phone,
-#     format: :international,
-#     spaces: ''
-#   ).gsub(/\s+/, "")
-# end
-
-# post '/login' do
-#   @user = User.find_by(username: params[:username])
-#   if @user && params[:password] == @user.password
-#     session["user_id"] = @user.id
-#     redirect "/plants/#{@user.id}"
-#   else
-#     erb :'index'
-#   end 
-# end
 
 post '/login' do
  
@@ -37,16 +18,14 @@ post '/login' do
   end
 end
 
-
-
-get '/plants' do
-  @user_plants = Plant.where(user_id: session["user_id"])
-  erb :'plants/show'
-end
-
 get '/sign-out' do
   session.clear
   redirect '/'
+end
+
+get '/plants' do
+  @user_plants = Plant.where(user_id: session["user_id"]).order(next_water_date: :asc)
+  erb :'plants/show'
 end
 
 delete '/plant-delete/:id' do
@@ -56,9 +35,35 @@ end
 
 post '/plant-update' do
   plant = Plant.find(params[:id])
+  plant.custom_name = params[:custom_name]
   plant.custom_watering_frequency = params[:custom_watering_frequency]
+  plant.last_date_watered = params[:last_date_watered]
+  plant.next_water_date = plant.calculate_next_water_date  
   plant.save
   redirect(back)
 end
 
+post '/plant-add' do
+  @new_plant = Plant.new(
+    user_id: session[:user_id],
+    plant_type_id: params[:plant_type_id],
+    custom_name: params[:custom_name],
+    last_date_watered: params[:last_date_watered],
+    custom_watering_frequency: params[:custom_watering_frequency]
+  )
+  @new_plant.calculate_next_water_date
+  @new_plant.save
+  #if @new_plant validates, save
+  if @new_plant.save
+    if request.xhr?
+      json  :plant => {  new_plant:  @new_plant, 
+                         common_name: @new_plant.plant_type.common_name
+                      }
+    else
+      redirect(back)
+    end
+  else
+    erb :'plants/show'
+  end
+end
 
