22cf713d3bb9f3c0273483484e492012cbc45385
ceebs <cthiessen@gmail.com>
2016-05-23 00:58:06 +0000

    updated users population

--- lib/users_importer.rb
+++ lib/users_importer.rb
@@ -1,10 +1,9 @@
 class UsersImporter
 
   def import
-    @chad = User.create(username: "Chad", password: "11111111", email: "test4@test.com", phone: )
-    @user1 = User.create(username: "test1", password: "00000000", email: "test1@test.com", phone: "+16046174988", communication_method: "SMS")
-    @user2 = User.create(username: "test2", password: "00000000", email: "test2@test.com", phone: "+16046174988", communication_method: "SMS")
-    @user3 = User.create(username: "test3", password: "00000000", email: "test3@test.com", phone: "+16046174988", communication_method: "SMS")
+    @user1 = User.create(username: "Chad", password: "00000000", email: "test1@test.com", phone: "+16046174988", communication_method: "SMS")
+    @user2 = User.create(username: "Jae", password: "00000000", email: "test2@test.com", phone: "+16046174988", communication_method: "SMS")
+    @user3 = User.create(username: "Chris", password: "00000000", email: "test3@test.com", phone: "+16046174988", communication_method: "SMS")
   end
 
 end
